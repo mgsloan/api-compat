3833b9b9c2fefc931a5477f141267899fa4522b6
mgsloan <mgsloan@gmail.com>
2012-07-22 16:53:58 -0700

    Cleaned up code and outputted API specs

--- src/ApiCompat/GetApi.hs
+++ src/ApiCompat/GetApi.hs
@@ -4,10 +4,9 @@ module ApiCompat.GetApi
   ( getApi ) where
 
 import Control.Applicative                 ( (<$>) )
-import Control.Arrow                       ( (***) )
 import Control.Monad.Trans                 ( liftIO )
-import Data.Either                         ( either )
-import Data.Char                           ( isAlphaNum, toLower, isSpace )
+import Control.Exception                   ( finally )
+import Data.Char                           ( isAlphaNum )
 import Data.List                           ( intersperse )
 import Language.Haskell.Interpreter        ( ModuleElem(..), getModuleExports, loadModules, interpret, reset, setTopLevelModules )
 import Language.Haskell.Interpreter.Unsafe ( unsafeRunInterpreterWithArgs )
@@ -15,43 +14,44 @@ import Language.Haskell.Interpreter.Unsafe ( unsafeRunInterpreterWithArgs )
 import System.Directory                    ( removeFile )
 import System.FilePath                     ( takeFileName, dropExtension )
 import System.IO                           ( openTempFile, hClose, hPutStr )
+import System.Process                      ( readProcess )
 
-getApi pkgs mods
-  = fail_left =<< unsafeRunInterpreterWithArgs (map ("-package "++) pkgs) exec
+type PackageName = String
+type ModuleName = String
+
+getApi :: [PackageName] -> [ModuleName] -> IO String
+getApi pkgs mods = do
+  (path, handle) <- openTempFile "." "GetApi.hs"
+
+  let args = map ("-package=" ++) pkgs
+
+  results <- (`finally` removeFile path) .
+    unsafeRunInterpreterWithArgs args $ do
+      splices <- mapM mk_splice mods
+
+      let mod_name = filter isAlphaNum . takeFileName $ dropExtension path
+
+          source = unlines
+            $ [ "{-# LANGUAGE TemplateHaskell #-}"
+              , "module " ++ mod_name ++ " where"
+              , "import ApiCompat.ShowInfo ( showInfos )"
+              , "import Prelude ( putStr, unlines, concat, ($), Char )"
+              ]
+           ++ map ("import " ++) mods
+           ++ [ "", "main = putStr output" ]
+           ++ [ "", "output = $(showInfos", "  [" ]
+           ++ zipWith (++) ("    " : repeat "  , ") splices
+           ++ [ "  ] )" ]
+
+      liftIO $ do
+        hPutStr handle source
+        hClose handle
+
+      liftIO $ readProcess "runhaskell" (args ++ [path]) ""
+
+  either (fail . show) return results
  where
-  exec = do
-    splices <- mapM mk_splice mods
-
-    (path, handle) <- liftIO $ openTempFile "." "GetApi.hs"
-
-
-    let mod_name = filter isAlphaNum . takeFileName $ dropExtension path
-
-        source = unlines
-          $ [ "{-# LANGUAGE TemplateHaskell #-}"
-            , "module " ++ mod_name ++ " where"
-            , "import ApiCompat.ShowInfo ( showInfo )"
-            , "import Prelude ( putStr, unlines, concat, ($), Char )"
-            ]
-         ++ map ("import " ++) mods
-         ++ [ "", "main = putStr output" ]
-         ++ [ "", "output = unlines $ concat", "  [" ]
-         ++ zipWith (++) ("    " : repeat "  , ") splices
-         ++ [ "  ]" ]
-
-    liftIO $ do
-      hPutStr handle source
-      hClose handle
-  
-    loadModules [path]
-    setTopLevelModules [mod_name]
-    result <- interpret (mod_name ++ ".output") ""
-    reset
-    liftIO $ removeFile path
-    return result
-
-  fail_left = either (fail . show) return
-  mk_splice m = (("$(showInfo \"" ++ m ++ "\" [") ++) . (++ "] )")
+  mk_splice m = (("(\"" ++ m ++ "\", [") ++) . (++ "] )")
               . concat . intersperse ", " . map process
             <$> getModuleExports m
    where
--- src/ApiCompat/ShowInfo.hs
+++ src/ApiCompat/ShowInfo.hs
@@ -1,123 +1,155 @@
+{-# LANGUAGE PatternGuards, ParallelListComp, TemplateHaskell #-}
 module ApiCompat.ShowInfo where
 
-import Control.Applicative                 ( (<$>) )
-import Data.List                           ( sort )
+import Control.Applicative           ( (<$>) )
+import Data.Char                     ( isAlphaNum )
+import Data.Function                 ( on )
+import Data.Generics                 ( Data, Typeable, listify, everywhere )
+import Data.Generics.Aliases         ( extT )
+import Data.List                     ( sort, groupBy, intersperse )
+import qualified Data.Map as M
 import Language.Haskell.TH
-import Language.Haskell.TH.Lift            ( lift )
-import Language.Haskell.TH.Instances
-import Language.Haskell.TH.Ppr
+import Language.Haskell.TH.Lift      ( lift )
+import Language.Haskell.TH.Instances ( )
 import Language.Haskell.TH.PprLib
+import Language.Haskell.TH.Syntax    (Name(..), NameFlavour(..))
 
+--TODO: remove contexts from methods
+--TODO: list instances next to classes / datatypes
 
-showInfo :: String -> [Name] -> ExpQ
-showInfo m ns = do
-  infos <- mapM safe_reify ns
-  lift $ [ "----------------------------------------"
-         , "module " ++ m ]
-      ++ map (either id (("\n"++) . show . display)) (sort infos)
-      ++ [""]
-  
+showInfos :: [(String, [Name])] -> ExpQ
+showInfos ms = lift . unlines . concat =<< mapM show_info ms
  where
+  show_info (m, ns) = do
+    infos <- mapM safe_reify ns
+    return
+      $ [ "----------------------------------------"
+        , "module " ++ m
+        ] ++ map (either id (("\n"++) . show . infoDoc)) (sort infos)
+          ++ [""]
   safe_reify n = recover (return . Left $ "-- Couldn't reify " ++ pprint n)
                          (Right <$> reify n)
 
-display info = case info of
-  (ClassI d _)                 -> d_dec d
---  display (ClassOpI)         ->
-  (TyConI d)                   -> d_dec d
-  (FamilyI d _)                -> d_dec d
---  display (PrimTyConI n i b) ->
---  display (DataConI )        ->
-  (VarI n t _ f)               -> vcat [d_fixity f n, d_dec $ SigD n t]
---  display (TyVarI n t)       ->
+infoDoc :: Info -> Doc
+infoDoc info = case info of
+  (ClassI d _)                 -> dec $ deUnique d
+--  (ClassOpI)                 ->
+  (TyConI d)                   -> dec $ deUnique d
+  (FamilyI d _)                -> dec $ deUnique d
+--  (PrimTyConI n i b)         ->
+--  (DataConI )                ->
+  (VarI n t _ f)               -> vcat [fixity f n, dec . SigD n $ deUnique t]
+--  (TyVarI n t)               ->
   _                            -> empty
  where
-  d_tvs = hcat . punctuate space . map ppr
+  tyvars = hcat . punctuate space . map ppr
 
   commaize = hcat . punctuate (text ", ")
 
-  d_name n = parens $ ppr n
+  vnest l = (l $+$) . nest 2 . vcat
 
-  d_ctx = parens . commaize . map d_pred . sort
+  name n | all (not . isAlphaNum) $ nameBase n = parens $ ppr n
+         | otherwise = ppr n
 
-  d_deriving = parens . commaize . map ppr . sort
+  prd (ClassP n ts) = hcat $ punctuate space (name n : map flat_typ ts)
+  prd (EqualP l r) = flat_typ l <+> text "~" <+> flat_typ r
 
-  v_infix f s xs = vcat $ map ((<+> text s) . f) (init xs) ++ [f $ last xs]
+  prds [] = text "()"
+  prds xs = parens . foldl1 ($$)
+          $ [nl] ++ (intersperse (text ",") . map prd $ sort xs) ++ [nl]
+   where nl = text ""
 
-  d_pred (ClassP n ts) = hcat $ punctuate space (d_name n : map d_flat_typ ts)
-  d_pred (EqualP l r) = d_flat_typ l <+> text " ~ " <+> d_flat_typ r
+  context = (<+> text "=>") . prds
 
-  d_forall (ForallT tvs ctx t) = hcat
-    [ text "forall ", d_tvs tvs, text ". ", d_ctx ctx, text " => " ]
+  derivings = parens . commaize . map ppr . sort
 
-  d_flat_typ ft@(ForallT _ _ t) = parens $ hcat [d_forall ft, d_flat_typ t]
-  d_flat_typ (SigT t k)         = parens $ hcat [d_flat_typ t, text "::", ppr k]
-  d_flat_typ (VarT n)           = d_name n
-  d_flat_typ (ConT n)           = d_name n
-  d_flat_typ ArrowT             = text "(->)"
-  d_flat_typ ListT              = text "[]"
-  d_flat_typ (AppT ListT r)     = brackets $ d_flat_typ r
-  d_flat_typ (AppT (AppT ArrowT l) r)
-    = parens . hcat $ punctuate (text " -> ") $ map d_flat_typ ts
---    = hcat [d_flat_typ l, text " ->", d_flat_typ r]
+  forall tvs ctx = hcat [ text "forall ", tyvars tvs, text ". " ]
+               $+$ context ctx
+
+  flat_typ (ForallT v c t) = parens $ hcat [forall v c, flat_typ t]
+  flat_typ (SigT t k)      = parens $ hcat [flat_typ t, text "::", ppr k]
+  flat_typ (VarT n)        = name n
+  flat_typ (ConT n)        = name n
+  flat_typ ArrowT          = text "(->)"
+  flat_typ ListT           = text "[]"
+  flat_typ (AppT ListT r)  = brackets $ flat_typ r
+  flat_typ t@(AppT (AppT ArrowT _) _)
+    = parens . hcat $ punctuate (text " -> ") $ map flat_typ ts
+--    = hcat [flat_typ l, text " ->", flat_typ r]
    where
-    ts = tyUnArrow r
-  d_flat_typ (AppT ArrowT l) = parens $ hcat [d_flat_typ l, text " ->"]
-  d_flat_typ t = case f of
+    ts = tyUnArrow t
+  flat_typ (AppT ArrowT l) = parens $ hcat [flat_typ l, text " ->"]
+  flat_typ t = case f of
     (TupleT        i) -> parens (tup_txt i)
     (UnboxedTupleT i) -> parens $ hcat [text "# ", tup_txt i, text " #"]
-    t                 -> d_flat_typ t
+    _                 -> hcat $ punctuate space flats
    where
     (f:rs) = tyUnApp t
-    flats = map d_flat_typ rs
-    tup_txt i = hcat [commaize flats, text $ replicate (i - length flats) ',']
+    flats = map flat_typ rs
+    tup_txt i = commaize flats <> text (replicate (i - length flats) ',')
 
-  d_typ (ForallT tvs ctx t) = vcat
-    [ hcat
-        [ text "forall ", d_tvs tvs, text ". ", d_ctx ctx, text " =>"
-        ]
-    , nest 2 $ d_typ t
-    ]
+  typ (ForallT tvs ctx t) 
+    =   hcat [text "forall ", tyvars tvs, text "."]
+    $+$ nest 2 (context ctx $+$ typ t)
 
-  d_typ t = v_infix d_flat_typ "->" $ tyUnArrow t
+  typ t = vb_infix flat_typ "->" $ tyUnArrow t
 
-  d_dec (ClassD ctx n tvs fds decs) = vcat
-    [ text "class" <+> d_ctx ctx
-    , hcat [text " => ", d_name n, space, d_tvs tvs, pp_fds, text " where"]
-    , nest 2 . vcat $ map d_dec decs
+  vb_infix f s xs
+    = vcat $ map ((<+> text s) . f) (init xs) ++ [f $ last xs]
+
+  vf_infix _ _ [] = empty
+  vf_infix f s (x:xs)
+    = vcat $ text (replicate (length s) ' ') <+> f x
+           : map ((text s <+>) . f) xs
+
+  dec (ClassD ctx n tvs fds decs) = vnest (text "class" <+> context ctx)
+    [ vnest (hcat [name n, space, tyvars tvs, pp_fds, text " where"])
+            (map dec decs)
     ]
    where
     pp_fds | [] <- fds = empty
            | otherwise = text "|" <+> commaize (map ppr fds)
 
-  d_dec (SigD n t) = d_name n <+> text "::" $$ nest 2 (d_typ t)
+  dec (SigD n t) = name n <+> text "::" $$ nest 2 (typ t)
 
-  d_dec (DataD    ctx n tvs cs ds) = d_data "data "    ctx n tvs  cs  ds
+  dec (DataD    ctx n tvs cs ds) = datalike "data "    ctx n tvs  cs  ds
 
-  d_dec (NewtypeD ctx n tvs cs ds) = d_data "newtype " ctx n tvs [cs] ds
+  dec (NewtypeD ctx n tvs cs ds) = datalike "newtype " ctx n tvs [cs] ds
 
---  d_dec (TySynD n tvs t) = hcat 
+--  dec (TySynD n tvs t) = hcat 
 
-  d_dec _ = empty
+  dec _ = empty
 
-  d_con = ppr
+  strict s = text $ case s of
+    NotStrict -> ""
+    IsStrict  -> "!"
+    Unpacked -> "{-# UNPACK #-} !"
 
-  d_data d ctx n tvs cs ds = vcat
-    [ text d, d_ctx ctx
-    , hcat [text " => ", d_name n, space, d_tvs tvs, enull $ text " ="]
-    , enull $ v_infix d_con "|" cs
-    , text "deriving " <+> d_deriving ds
+  field (NotStrict, t) = flat_typ t
+  field (s,         t) = strict s <+> parens (flat_typ t)
+
+  rec_field (n, s, t) = name n <+> text "::" <+> field (s, t)
+
+  con (NormalC n fs)      = vnest (name n) $ map field fs
+  con (RecC    n fs)      = vnest (name n) $ map rec_field fs
+  con (InfixC l n r)      = vnest (name n) $ map field [l, r]
+  con (ForallC tvs ctx c) = vnest (forall tvs ctx) [con c]
+
+  datalike d ctx n tvs cs ds = vnest (hcat [text d, context ctx])
+    [ hcat [name n, space, tyvars tvs, enull $ text " ="]
+    , enull $ vf_infix con "|" cs
+    , text "deriving" <+> derivings ds
     ]
    where
     enull x = if null cs then empty else x
 
-  d_fixity (Fixity l d) n
-    = text $ concat [ "infix", dir, " ", show l, " ", show $ d_name n ]
+  fixity (Fixity l d) n
+    = text $ concat [ "infix", dir, " ", show l, " ", show $ name n ]
    where
     dir = case d of
       InfixL -> "l"
       InfixR -> "r"
-      InfixN -> "" 
+      InfixN -> ""
 
 tyUnApp :: Type -> [Type]
 tyUnApp = reverse . helper
@@ -130,3 +162,30 @@ tyUnArrow = helper
  where
   helper (AppT (AppT ArrowT l) r) = l : helper r
   helper x                        = [x]
+
+everywhereMaybe :: (Typeable a, Data b) => (a -> Maybe a) -> b -> b
+everywhereMaybe f = everywhere (id `extT` replace)
+ where
+  replace x | Just x' <- f x = x'
+            | otherwise      = x
+
+deUnique :: Data a => a -> a
+deUnique d = everywhereMaybe (`M.lookup` rewrites) d
+ where
+  rewrites
+    = M.fromList . concatMap (process . map deUniqueName)
+    . groupBy ((==) `on` nameBase) $ allNames d
+
+  process [n] = [n]
+  process ns 
+    = [ (n, mkName $ nameBase n' ++ replicate p '\'')
+      | (n, n') <- ns
+      | p <- [0..]
+      ]
+
+  deUniqueName n@(Name o (NameU _)) = (n, Name o NameS)
+  deUniqueName n                    = (n, n)
+
+allNames :: Data a => a -> [Name]
+allNames = map head . groupBy (==) . sort
+         . listify (const True :: Name -> Bool)
